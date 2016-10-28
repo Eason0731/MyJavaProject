a03b3875624e87ed098c0e677041b8fdca801e70
lonlon29 <lonlon29@qq.com>
2016-10-28 17:31:51 +0800

    Update PopSort2 class

--- src/MyPopSort.java
+++ src/MyPopSort.java
@@ -3,14 +3,14 @@ public class MyPopSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int numberlist[] = {7,9,5,4,3,6,5,4,10,21};
+		int numberlist[] = {8,3,4,5,25,6,7,11};
 		for (int i = 0 ; i < numberlist.length ; i++ ){
 			for (int j = i+1 ; j < numberlist.length ; j++ ){
 				int temp;
-				if (i < j){
+				if (numberlist[i] < numberlist[j]){
 					temp = numberlist[i];
-					numberlist[j] = numberlist[i];
-					numberlist[i] = temp;
+					numberlist[i] = numberlist[j];
+					numberlist[j] = temp;
 				}
 			
 				
@@ -19,6 +19,7 @@ public class MyPopSort {
 			System.out.println(numberlist[i]);
 		}
 		
+	
 
 	}
 
