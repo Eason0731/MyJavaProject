6981c3fb7534e88d83f81564df22fe0aafc51d51
lonlon29 <lonlon29@qq.com>
2016-10-31 13:04:29 +0800

    Replace McLaren as Honda on MyPopSort2 class on Local branch

--- src/com/love/test/MyPopSort2.java
+++ src/com/love/test/MyPopSort2.java
@@ -4,18 +4,18 @@ public class MyPopSort2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int McLaren[] = {4,66,21,1,5,7,3};
-		for (int i = 0 ; i < McLaren.length; i++){
-			for(int j = i+1 ; j < McLaren.length; j++){
-				if(McLaren[i] < McLaren[j]){
+		int Honda[] = {4,66,21,1,5,7,3};
+		for (int i = 0 ; i < Honda.length; i++){
+			for(int j = i+1 ; j < Honda.length; j++){
+				if(Honda[i] < Honda[j]){
 					int temp;
-					temp = McLaren[i];
-					McLaren[i] = McLaren [j];
-					McLaren[j] = temp;
+					temp = Honda[i];
+					Honda[i] = Honda [j];
+					Honda[j] = temp;
 				}
 			}
 			
-			System.out.println(McLaren[i]);
+			System.out.println(Honda[i]);
 		}
 		
 	}
