5f8a434b4109d1be05877427065f62d786eebbb7
john0731 <john.zhang0731@outlook.com>
2017-03-20 14:40:51 +0800

    Update fixed values to input for PopSort on Java

--- src/com/love/test/MyPopSort2.java
+++ src/com/love/test/MyPopSort2.java
@@ -1,5 +1,7 @@
 package com.love.test;
 
+import java.util.Scanner;
+
 public class MyPopSort2 {
 	
 	public static void PopByDESC(int myNumberList[]){
@@ -36,10 +38,23 @@ public class MyPopSort2 {
 	}
 
 	public static void main(String[] args) {
-		int myNumberList[] = {21,8,5,1,7,3,14};		
+		//int myNumberList[] = {21,8,5,1,7,3,14};		
+		
+		System.out.println("你想输入几个数字?");
+		Scanner scanner_sz =new Scanner(System.in);
+		int sz =scanner_sz.nextInt();
+		
+		int i;
+		System.out.println("请输入" +sz+ "个整数 (每输入完一个数字请打回车)");
+		int[] myNumberList = new int[sz];
+		
+		for(i=0;i<sz;i++){
+			Scanner scanner_NumberList=new Scanner(System.in);
+			myNumberList[i]=scanner_NumberList.nextInt();		
+		}
+	
 		PopByASC(myNumberList);
 		PopByDESC(myNumberList);
-
 		
 	}
 
