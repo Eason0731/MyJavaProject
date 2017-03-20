246029cab65122054a0b9dbcaa274c470521d82d
john0731 <john.zhang0731@outlook.com>
2017-03-20 13:51:05 +0800

    Adding two PopSort method (DESC and ASC) for Java

--- src/com/love/test/MyPopSort2.java
+++ src/com/love/test/MyPopSort2.java
@@ -1,22 +1,45 @@
 package com.love.test;
 
 public class MyPopSort2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int Honda[] = {4,66,21,1,5,7,3};
-		for (int i = 0 ; i < Honda.length; i++){
-			for(int j = i+1 ; j < Honda.length; j++){
-				if(Honda[i] < Honda[j]){
-					int temp;
-					temp = Honda[i];
-					Honda[i] = Honda [j];
-					Honda[j] = temp;
+	
+	public static void PopByDESC(int myNumberList[]){
+		int tmp;
+		System.out.println("DESC order is: ");
+		for (int i=0 ; i<myNumberList.length; i++){
+			for (int j=i+1; j<myNumberList.length; j++){
+				if (myNumberList[i] < myNumberList[j]){
+					tmp = myNumberList[i];
+					myNumberList[i] = myNumberList[j];
+					myNumberList[j] = tmp;
 				}
 			}
-			
-			System.out.println(Honda[i]);
+			System.out.println( myNumberList[i]);	
 		}
+		System.out.println("================================");
+		
+	}
+	
+	public static void PopByASC(int[] myNumberList){
+		int tmp;
+		System.out.println("ASC order is: ");
+		for (int i=0 ; i<myNumberList.length; i++){
+			for (int j=i+1; j<myNumberList.length; j++){
+				if (myNumberList[i] > myNumberList[j]){
+					tmp = myNumberList[i];
+					myNumberList[i] = myNumberList[j];
+					myNumberList[j] = tmp;
+				}
+			}
+			System.out.println( myNumberList[i]);	
+		}
+		System.out.println("================================");
+	}
+
+	public static void main(String[] args) {
+		int myNumberList[] = {21,8,5,1,7,3,14};		
+		PopByASC(myNumberList);
+		PopByDESC(myNumberList);
+
 		
 	}
 
